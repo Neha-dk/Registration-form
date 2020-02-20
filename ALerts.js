4599470a884a62fe0ed180de16cc328fc58d1fc9
neha-d <nduggirala@cardlytic.com>
2020-02-20 11:42:48 +0530

    Added welcome alert to registration

---
+++ ALerts.js
@@ -0,0 +1,9 @@
+function Display(value)
+{
+    message+=value;
+}
+message = " ";
+var employee_name = prompt("Enter your name");
+to_be_alerted = ["Welcome ",employee_name," To the Resgistration Form"];
+to_be_alerted.forEach(Display);
+alert(message);
