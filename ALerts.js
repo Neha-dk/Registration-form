376e2c938deca4daf843bf56d0c8830dee02ff12
neha-d <nduggirala@cardlytic.com>
2020-02-21 10:57:10 +0530

    Date Addition

--- ALerts.js
+++ ALerts.js
@@ -7,3 +7,5 @@ var employee_name = prompt("Enter your name");
 to_be_alerted = ["Welcome ",employee_name," To the Resgistration Form"];
 to_be_alerted.forEach(Display);
 alert(message);
+obj = new Date();
+document.getElementById("date_day").innerHTML = obj.getFullYear();
